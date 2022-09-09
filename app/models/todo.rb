b0e4cbbdbe26ceb9a000ae4bed437c72163cc52e
Felice Forby <felice.forby@beam.dental>
2022-09-09 13:06:39 -0400

    Real-time updates with turbo stream

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,4 +2,9 @@ class Todo < ApplicationRecord
   validates :summary, presence: true
 
   scope :ordered, -> { order(created_at: :desc) }
+
+  # after_create_commit -> { broadcast_prepend_later_to "todos" }
+  # after_update_commit -> { broadcast_replace_later_to "todos" }
+  # after_destroy_commit -> { broadcast_remove_to "todos" }
+  broadcasts_to ->(todo) { "todos" }, inserts_by: :prepend
 end
