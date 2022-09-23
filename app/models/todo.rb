db5dd740eada80a0ab9a1820d304c8bafdbd9fc1
Felice Forby <felice.forby@beam.dental>
2022-09-23 11:45:04 -0400

    Make sure user outside share groups can't see other groups' todos

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -8,5 +8,5 @@ class Todo < ApplicationRecord
   # after_create_commit -> { broadcast_prepend_later_to "todos" }
   # after_update_commit -> { broadcast_replace_later_to "todos" }
   # after_destroy_commit -> { broadcast_remove_to "todos" }
-  broadcasts_to ->(todo) { "todos" }, inserts_by: :prepend
+  broadcasts_to ->(todo) { [todo.share_group, "todos"] }, inserts_by: :prepend
 end
